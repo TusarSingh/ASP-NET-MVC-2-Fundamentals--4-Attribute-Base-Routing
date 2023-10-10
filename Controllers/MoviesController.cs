4bf8c915448ff4bdcc2e1294179382b8787c5b80
Tusar Singh <rts0941@gmail.com>
2023-10-10 14:43:47 +0530

    Attribute Base Routing

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Tarun" };
+
+            return View(movie);
+
+        }
+
+        public ActionResult ByReleaseDate(int? Year, int Month)
+        {
+            return Content(Year + "/" + Month);
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseMonth(int? Year, int? Month)
+        {
+            return Content(Year + "/" + Month);
+        }
+
+        // /{month:regex(\\d{4}):range(1,12)}
+
+    }
+}
